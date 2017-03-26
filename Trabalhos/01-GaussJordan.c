8cb8758c1a7286c6231f3152a03ea6d2fda94e15
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-03-26 00:03:26 -0300

    Testando leitura e impressão de double

--- Trabalhos/01-GaussJordan.c
+++ Trabalhos/01-GaussJordan.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("hi\nhehe\noops\n");
+  double size_a;
+  scanf("%lf", &size_a);
+  //double *a = (double *) malloc(sizeof(double) * );
+  printf("%.5lf\n", size_a);
   return 0;
 }
