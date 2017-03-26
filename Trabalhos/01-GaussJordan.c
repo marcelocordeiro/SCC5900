5912705abde8e1f39ccbe012072bf5692dea8085
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-03-26 00:37:54 -0300

    Lendo matriz A e vetor B

--- Trabalhos/01-GaussJordan.c
+++ Trabalhos/01-GaussJordan.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  double size_a;
-  scanf("%lf", &size_a);
-  //double *a = (double *) malloc(sizeof(double) * );
-  printf("%.5lf\n", size_a);
+  int size_a;
+  double **a, *b, *x;
+  scanf("%d", &size_a);
+  b = malloc (size_a * sizeof(double));
+  x = malloc (size_a * sizeof(double));
+  a = malloc (size_a * sizeof(double));
+  for (int i=0; i < size_a; i++) {
+    a[i] = malloc(size_a * sizeof(double));
+    for (int j=0; j < size_a; j++) {
+      scanf("%lf", &a[i][j]);
+    }
+  }
+  for (int i=0; i < size_a; i++) {
+    scanf("%lf", &b[i]);
+  }
+
+  free(a);
+  free(b);
+  free(x);
+  b = x = NULL;
+  for (int i=0; i < size_a; i++)
+    a[i] = NULL;
   return 0;
 }
