6fd3c1f1f0131d97d911a8cf4461e580b5502071
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-03-26 02:28:07 -0300

    Quase funcionando... Acho que falta troca de linha

--- Trabalhos/01-GaussJordan.c
+++ Trabalhos/01-GaussJordan.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void GaussJordan (int size, double **a, double *b, double *x) {
-  x[0] = 3;
-  x[1] = 1;
-  x[2] = 2;
+void GaussJordan (int size, double **a, double *b) {
+  for (int i=0; i < size; i++) {
+    if (a[i][i] == 0) { //É necessário trocar a linha
+      //Troca de linha
+    }
+    if (a[i][i] != 1) { //É necessário pivotar a linha
+      //("Bora pivotar\n");
+      int pivo = a[i][i];
+      for (int j=0; j < size; j++) {
+        //printf("Dividindo %.0lf por %.0lf\n", a[i][j], a[i][i]);
+        a[i][j] /= pivo; //Dividindo todos da linha pelo pivô
+      }
+      b[i] /= pivo;
+    }
+    //Linha pivotada!
+    //Próximo passo: zerar a coluna do pivô
+    for (int j = 0; j < size; j++) {
+      if (j != i) {
+        double multiplier = a[j][i];
+        for (int k = 0; k < size; k++) {
+          a[j][k] -= (multiplier*a[i][k]);
+        }
+        b[j] -= (multiplier*b[i]);
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
   int size;
-  double **a, *b, *x;
+  double **a, *b;
   scanf("%d", &size);
   b = malloc (size * sizeof(double));
-  x = malloc (size * sizeof(double));
   a = malloc (size * sizeof(double));
   for (int i=0; i < size; i++) {
     a[i] = malloc(size * sizeof(double));
@@ -24,16 +45,15 @@ int main(int argc, char *argv[]) {
     scanf("%lf", &b[i]);
   }
 
-  GaussJordan(size, a, b, x);
+  GaussJordan(size, a, b);
 
   for (int i=0; i < size; i++) {
-    printf("%.5lf\n", x[i]);
+    printf("%.5lf\n", b[i]);
   }
 
   free(a);
   free(b);
-  free(x);
-  b = x = NULL;
+  b = NULL;
   for (int i=0; i < size; i++)
     a[i] = NULL;
   return 0;
