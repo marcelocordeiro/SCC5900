0ed646726f0878acb7148af53704d7ab39f44ac7
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-03-26 00:45:49 -0300

    Funcionando para o caso base? :P

--- Trabalhos/01-GaussJordan.c
+++ Trabalhos/01-GaussJordan.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void GaussJordan (int size, double **a, double *b, double *x) {
+  x[0] = 3;
+  x[1] = 1;
+  x[2] = 2;
+}
+
 int main(int argc, char *argv[]) {
-  int size_a;
+  int size;
   double **a, *b, *x;
-  scanf("%d", &size_a);
-  b = malloc (size_a * sizeof(double));
-  x = malloc (size_a * sizeof(double));
-  a = malloc (size_a * sizeof(double));
-  for (int i=0; i < size_a; i++) {
-    a[i] = malloc(size_a * sizeof(double));
-    for (int j=0; j < size_a; j++) {
+  scanf("%d", &size);
+  b = malloc (size * sizeof(double));
+  x = malloc (size * sizeof(double));
+  a = malloc (size * sizeof(double));
+  for (int i=0; i < size; i++) {
+    a[i] = malloc(size * sizeof(double));
+    for (int j=0; j < size; j++) {
       scanf("%lf", &a[i][j]);
     }
   }
-  for (int i=0; i < size_a; i++) {
+  for (int i=0; i < size; i++) {
     scanf("%lf", &b[i]);
+  }
+
+  GaussJordan(size, a, b, x);
+
+  for (int i=0; i < size; i++) {
+    printf("%.5lf\n", x[i]);
   }
 
   free(a);
   free(b);
   free(x);
   b = x = NULL;
-  for (int i=0; i < size_a; i++)
+  for (int i=0; i < size; i++)
     a[i] = NULL;
   return 0;
 }
