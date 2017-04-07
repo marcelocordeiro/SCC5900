953f3c48188fca19600644e50a7bb92fca7207fb
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-04-07 06:12:46 -0300

    Exercicio agenda entregue

---
+++ Exercicios/01-Agenda.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  struct {
+    int dia, mes, ano, hora, minuto, segundo;
+    char desc[100];
+  } agenda;
+  int cont;
+
+  scanf("%d", &cont);
+  for (int i=0; i < cont; i++) {
+    scanf("%d", &agenda.dia);
+    scanf("%d", &agenda.mes);
+    scanf("%d", &agenda.ano);
+    scanf("%d", &agenda.hora);
+    scanf("%d", &agenda.minuto);
+    scanf("%d", &agenda.segundo);
+    getchar();
+    fgets(agenda.desc, sizeof agenda.desc, stdin);
+    printf("%02d/%02d/%02d - %02d:%02d:%02d\n", agenda.dia, agenda.mes, agenda.ano, agenda.hora, agenda.minuto, agenda.segundo);
+    printf("%s", agenda.desc);
+  }
+  return 0;
+}
