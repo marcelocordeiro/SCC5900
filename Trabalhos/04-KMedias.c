ff9e44f18166b4c9bb75e04b7806cf330858c939
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-04-07 14:26:16 -0300

    Criado arquivo para K Médias

---
+++ Trabalhos/04-KMedias.c
@@ -0,0 +1,13 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  
+  return 0;
+}
