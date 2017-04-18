a3e7ee8d3e39f4f2072bfec79406f93d56491d03
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-04-18 00:06:31 -0300

    Lendo arquivo de audio para a transformada discreta do cosseno D:

---
+++ Trabalhos/05-TransfCosseno.c
@@ -0,0 +1,35 @@
+
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  FILE *audio;
+  unsigned char byte;
+  int k;
+  char nome_arquivo[20];
+
+  scanf("%s", nome_arquivo);
+  getchar();
+
+  scanf("%d", &k);
+
+  audio = fopen(nome_arquivo,"rb");
+  if (!audio) {
+    printf("Arquivo não encontrado\n");
+    return 1;
+  }
+
+  while (!feof(audio)) {
+    fread(&byte, 1, 1, audio);
+  }
+
+  fclose(audio);
+
+  return 0;
+}
