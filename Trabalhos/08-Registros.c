bc467596dd1317e607fb5bcf8da62a318d1409d0
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-05-08 20:10:11 -0300

    Insert lendo no union

--- Trabalhos/08-Registros.c
+++ Trabalhos/08-Registros.c
@@ -18,22 +18,22 @@ union tipos_dados {
 };
 
 struct estrutura {
-  union tipos_dados *uni;
+  union tipos_dados uni;
   int tipo; //0 = int, 1 = double, 2 = char; 3 = string; 4 = float;
 };
 
 int main(int argc, char *argv[]) {
   FILE *arquivo, *binario;
   struct estrutura *vetor;
-  char nome_meta[20], nome_arquivo[20], comando[6], aux1[20], aux2[20];
-  int cont, num_campos;
+  char nome_meta[20], nome_arquivo[20], comando[6], comando_insert[100], aux1[20], aux2[20], *token;
+  int cont, num_campos, num_elem, procura;
 
   scanf("%s", nome_meta);
   getchar();
 
   arquivo = fopen(nome_meta,"r");
   if (!arquivo) {
-    printf("File not found.\n");
+    printf("File %s not found.\n", nome_meta);
     return 1;
   }
 
@@ -77,39 +77,97 @@ int main(int argc, char *argv[]) {
     cont++;
   }
 
-  printf("Tenho %d campos\n", num_campos);
-  for (int i=0; i < num_campos; i++) {
-    printf("Campo %d: %d\n", i, vetor[i].tipo);
-  }
+  // printf("Tenho %d campos\n", num_campos);
+  // for (int i=0; i < num_campos; i++) {
+  //   printf("Campo %d: %d\n", i, vetor[i].tipo);
+  // }
 
   binario = fopen(nome_arquivo,"wb+");
+  if (!binario) {
+    printf("Unable to create file %s.\n", nome_arquivo);
+    return 1;
+  }
 
-  // scanf("%s", comando);
-  // getchar();
-  // while (strcmp(comando,"exit")!=0) {
-  //   for (int i=0; i < num_campos; i++) {
-  //     switch (vetor[i].tipo) {
-  //       case 0:
-  //         scanf("%d", &algo);
-  //         break;
-  //       case 1:
-  //         scanf("%lf", &algo);
-  //         break;
-  //       case 2:
-  //         scanf("%c", &algo);
-  //         break;
-  //       case 3:
-  //         scanf("%s", algo);
-  //         getchar();
-  //         break;
-  //       case 4:
-  //         scanf("%f", &algo);
-  //         break;
-  //     }
-  //   }
-  //   scanf("%s", comando);
-  //   getchar();
-  // }
+  //TODO
+  //Ler o arquivo binario para saber quantos elementos já existem nele, e então atualizar num_elem
+  num_elem = 0;
+
+  scanf("%s", comando);
+  getchar();
+  while (strcmp(comando,"exit")!=0) {
+    if (strcmp(comando,"search")==0) {
+      scanf("%d", &procura);
+      //Realizar procura
+    }
+    else {
+      if (strcmp(comando,"index")==0) {
+        //Criar index
+      }
+      else {
+        fgets(comando_insert, sizeof(comando_insert), stdin);
+        cont = 0;
+        token = strtok(comando_insert, ",");
+        while (token != 0) {
+          switch (vetor[cont].tipo) {
+            case 0:
+              vetor[cont].uni.i = atoi(token);
+              // printf("Int %d\n", vetor[cont].uni.i);
+              break;
+            case 1:
+              vetor[cont].uni.d = atof(token);
+              // printf("Double %lf\n", vetor[cont].uni.d);
+              break;
+            case 2:
+              vetor[cont].uni.c = token[1];
+              // printf("Char %c\n", vetor[cont].uni.c);
+              break;
+            case 3:
+              for (int i = 0; i < strlen(token); i++) {
+                if (token[i] == '"') {
+                  i++;
+                  while (token[i] != '"') {
+                    vetor[cont].uni.s[i-2] = token[i];
+                    i++;
+                  }
+                }
+              }
+              // printf("String %s\n", vetor[cont].uni.s);
+              break;
+            case 4:
+              vetor[cont].uni.f = (float)atof(token);
+              // printf("Float %f\n", vetor[cont].uni.f);
+              break;
+          }
+          cont++;
+          token = strtok(0, ",");
+        }
+        num_elem++;
+      }
+    }
+    // printf("Comando: %s\n%s\n", comando, comando_insert);
+    // for (int i=0; i < num_campos; i++) {
+    //   switch (vetor[i].tipo) {
+    //     case 0:
+    //       printf("%d\n", vetor[i].uni.i);
+    //       break;
+    //     case 1:
+    //       printf("%lf\n", vetor[i].uni.d);
+    //       break;
+    //     case 2:
+    //       printf("%c\n", vetor[i].uni.c);
+    //       break;
+    //     case 3:
+    //       printf("%s\n", vetor[i].uni.s);
+    //       getchar();
+    //       break;
+    //     case 4:
+    //       printf("%f\n", vetor[i].uni.f);
+    //       break;
+    //   }
+    // }
+    scanf("%s", comando);
+    getchar();
+  }
 
   fclose(binario);
   fclose(arquivo);
