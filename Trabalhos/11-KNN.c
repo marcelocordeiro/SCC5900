ff6bc61fdb4e944d4f0135d8bd549d36e0d8032c
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-05-13 18:09:40 -0300

    Gravando arquivo .data

--- Trabalhos/11-KNN.c
+++ Trabalhos/11-KNN.c
@@ -22,10 +22,10 @@ struct estrutura {
 };
 
 int main(int argc, char *argv[]) {
-  FILE *file_schema, file_data;
+  FILE *file_schema, *file_data;
   struct estrutura *schema;
   char nome_schema[30], nome_data[30], aux1[20], aux2[20];
-  int num_campos;
+  int num_campos, aux;
 
   scanf("%s", nome_schema);
   getchar();
@@ -38,17 +38,17 @@ int main(int argc, char *argv[]) {
 
   schema = malloc(sizeof(struct estrutura));
 
+  schema[0].tipo = 0; //ID
   num_campos = 0;
   while (fscanf(file_schema, "%s %s", aux1, aux2) > 0) {
-    if (num_campos == 0) {
+    num_campos++;
+    if (num_campos == 1) {
       strcpy(nome_data, aux2);
       strcat(nome_data,".data");
       printf("Nome do arquivo de dados: %s\n", nome_data);
     }
     else {
-      if (num_campos >= 2) {
-        schema = realloc(schema, num_campos * sizeof(struct estrutura));
-      }
+      schema = realloc(schema, num_campos * sizeof(struct estrutura));
       if (strcmp(aux2,"int") == 0) {
         schema[num_campos-1].tipo = 0;
       }
@@ -74,20 +74,70 @@ int main(int argc, char *argv[]) {
         }
       }
     }
-    num_campos++;
   }
-  num_campos -= 2;
+  schema = realloc(schema, (num_campos + 1) * sizeof(struct estrutura));
+  schema[num_campos].tipo = 1; //Dist
+  num_campos++;
 
-  printf("Num campos: %d + classe\n", num_campos);
+  printf("Num campos: %d\n", num_campos);
 
-  for (int i=0; i <= num_campos; i++) {
+  for (int i=0; i < num_campos; i++) {
     printf("Tipo: %d\n", schema[i].tipo);
   }
 
+  file_data = fopen(nome_data,"wb+");
+  if (!file_data) {
+    printf("Unable to create file %s.\n", nome_data);
+    return 1;
+  }
+
+  scanf("%d", &aux);
+  while (aux != -1) {
+    schema[0].uni.i = aux;
+    for (int i=1; i < (num_campos - 1); i++) {
+      switch (schema[i].tipo) {
+        case 0:
+          scanf("%d", &schema[i].uni.i);
+          break;
+        case 1:
+          scanf("%lf", &schema[i].uni.d);
+          break;
+        case 2:
+          getchar();
+          scanf("%[^\n]s", schema[i].uni.s);
+          break;
+      }
+    }
+    schema[num_campos].uni.d = 0;
+
+    printf("Vou gravar:\n");
+    for (int i=0; i < num_campos; i++) {
+      switch (schema[i].tipo) {
+        case 0:
+          printf("%d\n", schema[i].uni.i);
+          fwrite(&schema[i].uni.i, 1, sizeof(schema[i].uni.i), file_data);
+          break;
+        case 1:
+          printf("%lf\n", schema[i].uni.d);
+          fwrite(&schema[i].uni.d, 1, sizeof(schema[i].uni.d), file_data);
+          break;
+        case 2:
+          printf("%s\n", schema[i].uni.s);
+          fwrite(schema[i].uni.s, 1, sizeof(schema[i].uni.s), file_data);
+          break;
+      }
+    }
+
+    scanf("%d", &aux);
+  }
+
+  //Continue
+
   fclose(file_schema);
-  for (int i=0; i <= num_campos; i++) {
-    if (schema[i].tipo = 2) {
-      free (schema[i].uni.s);
+  fclose(file_data);
+  for (int i=0; i < num_campos; i++) {
+    if (schema[i].tipo == 2) {
+      free(schema[i].uni.s);
     }
   }
   free(schema);
