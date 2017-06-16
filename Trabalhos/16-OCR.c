9fc0c7f890817089db9daa8e589de4d2c241b61f
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-06-16 14:52:25 -0300

    OCR? O que é isso?

--- Trabalhos/16-OCR.c
+++ Trabalhos/16-OCR.c
@@ -4,3 +4,10 @@ Número USP: 10342032
 Turma: SCC5900 - Projeto de Algoritmos
 Data da entrega: 16/06/2017
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  return 0;
+}
