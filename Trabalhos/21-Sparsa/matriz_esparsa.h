ec049efa368d6d6256334274b2324223c7ac78b9
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-06-28 20:09:28 -0300

    Adição de arquivos .h e Makefile no gitignore

---
+++ Trabalhos/21-Sparsa/matriz_esparsa.h
@@ -0,0 +1,23 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 28/06/2017
+*/
+
+#ifndef MATRIZ_ESPARSA_H
+#define MATRIZ_ESPARSA_H
+
+typedef struct matriz_esparsa MATRIZ_ESPARSA;
+
+MATRIZ_ESPARSA *criar_matriz(int nr_linhas, int nr_colunas);
+void apagar_matriz(MATRIZ_ESPARSA **matriz);
+
+int set_matriz(MATRIZ_ESPARSA *matriz, int lin, int col, int val);
+
+MATRIZ_ESPARSA *multiplicar_matriz(MATRIZ_ESPARSA *m1, MATRIZ_ESPARSA *m2);
+MATRIZ_ESPARSA *somar_matriz(MATRIZ_ESPARSA *m1, MATRIZ_ESPARSA *m2);
+
+void imprimir_matriz(MATRIZ_ESPARSA *matriz);
+
+#endif /* MATRIZ_ESPARSA_H */
