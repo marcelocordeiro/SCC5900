ed398c48d59552479103da467b0e49ba5137e0f1
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-06-28 08:25:27 -0300

    Arquivos base

---
+++ Trabalhos/21-Sparsa/main.c
@@ -0,0 +1,47 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 28/06/2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "matriz_esparsa.h"
+
+int main(){
+    char op;
+    int nr_linhas, nr_colunas, valor;
+
+    //definindo a operação
+    scanf("%c", &op);
+
+    //lendo a matriz a
+    scanf("%d %d %d", &valor, &nr_linhas, &nr_colunas);
+    MATRIZ_ESPARSA * A = criar_matriz(nr_linhas, nr_colunas);
+    while (scanf("%d %d %d", &nr_linhas, &nr_colunas, &valor) && nr_linhas != -1){
+        set_matriz(A, nr_linhas, nr_colunas, valor);
+    }
+
+    //lendo a matriz b
+    MATRIZ_ESPARSA * B = criar_matriz(nr_colunas, valor);
+    while (scanf("%d %d %d", &nr_linhas, &nr_colunas, &valor) != EOF){
+        set_matriz(B, nr_linhas, nr_colunas, valor);
+    }
+
+    MATRIZ_ESPARSA *C = NULL;
+    if (op == ’M’){
+        C = multiplicar_matriz(A,B);
+    } else if (op == ’A’){
+        C = somar_matriz(A,B);
+    }
+
+    imprimir_matriz(C);
+
+    apagar_matriz(&A);
+    apagar_matriz(&B);
+    apagar_matriz(&C);
+
+    return 0;
+}
---
+++ Trabalhos/21-Sparsa/matriz_esparsa.c
@@ -0,0 +1,133 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 28/06/2017
+*/
+
+#include "matriz_esparsa.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct CELULA {
+    int linha;
+    int coluna;
+    int valor;
+    struct CELULA *direita;
+    struct CELULA *abaixo;
+} CELULA;
+
+struct matriz_esparsa {
+    CELULA **linhas;
+    CELULA **colunas;
+    int nr_linhas;
+    int nr_colunas;
+};
+
+MATRIZ_ESPARSA *criar_matriz(int nr_linhas, int nr_colunas) {
+    MATRIZ_ESPARSA *mat = (MATRIZ_ESPARSA *) malloc(sizeof (MATRIZ_ESPARSA));
+
+    if (mat != NULL) {
+        int i;
+        mat->nr_colunas = nr_colunas;
+        mat->nr_linhas = nr_linhas;
+        mat->colunas = (CELULA **) malloc(sizeof (CELULA *) * nr_colunas);
+        mat->linhas = (CELULA **) malloc(sizeof (CELULA *) * nr_linhas);
+
+        if (mat->colunas != NULL && mat->linhas != NULL) {
+            for (i = 0; i < nr_colunas; i++) {
+                mat->colunas[i] = (CELULA *) malloc(sizeof (CELULA));
+                mat->colunas[i]->abaixo = NULL;
+            }
+            for (i = 0; i < nr_linhas; i++) {
+                mat->linhas[i] = (CELULA *) malloc(sizeof (CELULA));
+                mat->linhas[i]->direita = NULL;
+            }
+        }
+    }
+    return mat;
+}
+
+void apagar_matriz(MATRIZ_ESPARSA **matriz) {
+    int i;
+    for (i = 0; i < (*matriz)->nr_linhas; i++) {
+        CELULA *paux = (*matriz)->linhas[i]->direita;
+        while (paux != NULL) {
+            CELULA *prem = paux;
+            paux = paux->direita;
+            free(prem);
+        }
+        free((*matriz)->linhas[i]);
+    }
+
+    free((*matriz)->linhas);
+    free((*matriz)->colunas);
+    free((*matriz));
+    *matriz = NULL;
+}
+
+int set_matriz(MATRIZ_ESPARSA *matriz, int lin, int col, int val) {
+    if (lin < matriz->nr_linhas && col < matriz->nr_colunas) {
+        CELULA *paux = matriz->linhas[lin];
+        while (paux->direita != NULL && paux->direita->coluna <= col) {
+            paux = paux->direita;
+        }
+
+        if (paux->coluna == col) {
+            paux->valor = val;
+        } else {
+            CELULA *pnovo = (CELULA *) malloc(sizeof (CELULA));
+            if (pnovo != NULL) {
+                pnovo->linha = lin;
+                pnovo->coluna = col;
+                pnovo->valor = val;
+                pnovo->direita = paux->direita;
+                paux->direita = pnovo;
+
+                paux = matriz->colunas[col];
+                while (paux->abaixo != NULL && paux->abaixo->linha <= lin) {
+                    paux = paux->abaixo;
+                }
+                pnovo->abaixo = paux->abaixo;
+                paux->abaixo = pnovo;
+            }
+        }
+        return 1;
+    }
+    return 0;
+}
+
+int get_matriz(MATRIZ_ESPARSA *matriz, int lin, int col) {
+    if (lin < matriz->nr_linhas && col < matriz->nr_colunas) {
+        CELULA *paux = matriz->linhas[lin];
+        while (paux->direita != NULL && paux->direita->coluna <= col) {
+            paux = paux->direita;
+        }
+
+        if (paux->coluna == col) {
+            return paux->valor;
+        }
+    }
+    return 0;
+}
+
+MATRIZ_ESPARSA *multiplicar_matriz(MATRIZ_ESPARSA *m1, MATRIZ_ESPARSA *m2) {
+    return NULL;
+}
+
+MATRIZ_ESPARSA *somar_matriz(MATRIZ_ESPARSA *m1, MATRIZ_ESPARSA *m2) {
+    return NULL;
+}
+
+void imprimir_matriz(MATRIZ_ESPARSA *matriz) {
+    int i;
+    CELULA *paux;
+
+    printf("-1 %d %d\n", matriz->nr_linhas; matriz->nr_colunas);
+    for (i = 0; i < matriz->nr_linhas; i++) {
+        for (paux = matriz->linhas[i]; paux != NULL; paux = paux->direita) {
+            printf("%d %d %d\n", paux->linha, paux->coluna, paux->valor);
+        }
+    }
+}
