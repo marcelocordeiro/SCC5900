fce7ac05bd5b0a2ff3e3d325df104f5ae7fcce46
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-07-04 08:41:48 -0300

    Sizeof

---
+++ Exercicios/65-Sizeof.c
@@ -0,0 +1,25 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 04/07/2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  printf("%d\n", (int) sizeof(char));
+  printf("%d\n", (int) sizeof(unsigned int));
+  printf("%d\n", (int) sizeof(short int));
+  printf("%d\n", (int) sizeof(int));
+  printf("%d\n", (int) sizeof(long int));
+  printf("%d\n", (int) sizeof(unsigned long int));
+  printf("%d\n", (int) sizeof(long long int));
+  printf("%d\n", (int) sizeof(float));
+  printf("%d\n", (int) sizeof(double));
+  printf("%d\n", (int) sizeof(long double));
+
+  return 0;
+}
