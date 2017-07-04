ccb8d69462682139bfe9b01879a09e34f01a6756
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-07-04 07:13:02 -0300

    Palavra palindrome

---
+++ Exercicios/53-WordPal.c
@@ -0,0 +1,27 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 04/07/2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char aux[5000];
+
+  scanf("%s", aux);
+
+  for (int i = 0; i < strlen(aux); i++) {
+    if (aux[i] != aux[strlen(aux)-1-i]) {
+      printf("NAO\n");
+      return 0;
+    }
+  }
+
+  printf("SIM\n");
+
+  return 0;
+}
