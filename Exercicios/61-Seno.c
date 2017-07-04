d74d20ad7e9bab25ff974076d69ef2d2d5d2b69b
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-07-04 08:17:10 -0300

    Seno

---
+++ Exercicios/61-Seno.c
@@ -0,0 +1,20 @@
+/*
+Nome: Marcelo Augusto Cordeiro
+Número USP: 10342032
+Turma: SCC5900 - Projeto de Algoritmos
+Data da entrega: 04/07/2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+  double angle;
+
+  scanf("%lf", &angle);
+
+  printf("%.6lf\n", sin(angle));
+
+  return 0;
+}
